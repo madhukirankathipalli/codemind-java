c18b7ed2b95a40e39372c609fd1302d8ba2cd2a5
KATHIPALLI MADHU KIRAN <119720465+madhukirankathipalli@users.noreply.github.com>
2023-06-14 23:42:46 +0530

    import java.util.Scanner; public class xyz { 	public static void main(String args[]) 	{ 		char c; 		Scanner sc=new Scanner(System.in); 		c=sc.nextLine().charAt(0); 		if(c=='V'|| c=='v') 		{ 		    System.out.println("Violet"); 		} 		else if(c=='I'|| c=='i') 		{ 		    System.out.println("Indigo"); 		} 		else if(c=='B'|| c=='b') 		{ 		    System.out.println("Blue"); 		} 		else if(c=='G' || c=='g') 		{ 		    System.out.println("Green"); 		} 		else if(c=='Y'|| c=='y') 		{ 		    System.out.println("Yellow"); 		} 		else if(c=='O'|| c=='o') 		{ 		    System.out.println("Orange"); 		} 		else if(c=='R'|| c=='r') 		{ 		    System.out.println("Red"); 		} 		else 		{ 		    System.out.println("-1"); 		} 		sc.close(); 	} }

---
+++ Color_Code.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+public class xyz
+{
+	public static void main(String args[])
+	{
+		char c;
+		Scanner sc=new Scanner(System.in);
+		c=sc.nextLine().charAt(0);
+		if(c=='V'|| c=='v')
+		{
+		    System.out.println("Violet");
+		}
+		else if(c=='I'|| c=='i')
+		{
+		    System.out.println("Indigo");
+		}
+		else if(c=='B'|| c=='b')
+		{
+		    System.out.println("Blue");
+		}
+		else if(c=='G' || c=='g')
+		{
+		    System.out.println("Green");
+		}
+		else if(c=='Y'|| c=='y')
+		{
+		    System.out.println("Yellow");
+		}
+		else if(c=='O'|| c=='o')
+		{
+		    System.out.println("Orange");
+		}
+		else if(c=='R'|| c=='r')
+		{
+		    System.out.println("Red");
+		}
+		else
+		{
+		    System.out.println("-1");
+		}
+		sc.close();
+	}
+}
