1ed7138a5588ac4fe8b7bc8b462b82bf3cd29e0e
KATHIPALLI MADHU KIRAN <119720465+madhukirankathipalli@users.noreply.github.com>
2023-06-14 23:29:23 +0530

    import java.util.Scanner; public class xyz { 	public static void main(String args[]) 	{ 		int a,b; 		Scanner sc=new Scanner(System.in); 		a=sc.nextInt(); 		b=sc.nextInt(); 		System.out.format("%d",a+b); 		sc.close(); 	} }

---
+++ Adding_Two_Numbers.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class xyz
+{
+	public static void main(String args[])
+	{
+		int a,b;
+		Scanner sc=new Scanner(System.in);
+		a=sc.nextInt();
+		b=sc.nextInt();
+		System.out.format("%d",a+b);	
+		sc.close();
+	}
+}
