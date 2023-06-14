b8b7888196d5a23d4be25ff4c2458922bd55d921
KATHIPALLI MADHU KIRAN <119720465+madhukirankathipalli@users.noreply.github.com>
2023-06-15 00:27:54 +0530

    import java.util.*; public class unique {     public static int count(int a[],int k)     {         int c=0;         for(int i=0;i<a.length;i++)         {             if(a[i]<k)             {                 c++;             }         }         return c;     }     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         int a[]=new int[n];         for(int i=0;i<n;i++)         {             a[i]=sc.nextInt();         }         for(int i=0;i<n;i++)         {             System.out.print(count(a,a[i])+" ");         }     } }

---
+++ How_Many_Numbers_Are_Smaller_Than_the_Current_Number.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class unique
+{
+    public static int count(int a[],int k)
+    {
+        int c=0;
+        for(int i=0;i<a.length;i++)
+        {
+            if(a[i]<k)
+            {
+                c++;
+            }
+        }
+        return c;
+    }
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int a[]=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            a[i]=sc.nextInt();
+        }
+        for(int i=0;i<n;i++)
+        {
+            System.out.print(count(a,a[i])+" ");
+        }
+    }
+}
