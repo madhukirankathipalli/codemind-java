6bb6ca0bc35f4e446be47dde86be4cce9f259ab5
KATHIPALLI MADHU KIRAN <119720465+madhukirankathipalli@users.noreply.github.com>
2023-06-14 23:29:04 +0530

    import java.util.Scanner; public class xyz { 	public static void main(String args[]) 	{ 	    String a; 		Scanner sc=new Scanner(System.in); 		a=sc.nextLine(); 		System.out.println(a); 		sc.close(); 	} }

---
+++ Take_Input_And_Print.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class xyz
+{
+	public static void main(String args[])
+	{
+	    String a;
+		Scanner sc=new Scanner(System.in);
+		a=sc.nextLine();
+		System.out.println(a);	
+		sc.close();
+	}
+}
