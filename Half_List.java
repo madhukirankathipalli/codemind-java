8383e9e4e488065c26d1829cd9bab572eaa2b303
KATHIPALLI MADHU KIRAN <119720465+madhukirankathipalli@users.noreply.github.com>
2023-06-15 11:18:11 +0530

    import java.util.*; public class sample {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         int [] arr=new int [n];         for (int i=0;i<n;i++)             arr[i]=sc.nextInt();         for (int i=n-1;i>=(int)n/2;i--)             System.out.print(arr[i]+" ");         for (int i=0;i<(int)n/2;i++)             System.out.print(arr[i]+" ");     } }

---
+++ Half_List.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class sample
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int [] arr=new int [n];
+        for (int i=0;i<n;i++)
+            arr[i]=sc.nextInt();
+        for (int i=n-1;i>=(int)n/2;i--)
+            System.out.print(arr[i]+" ");
+        for (int i=0;i<(int)n/2;i++)
+            System.out.print(arr[i]+" ");
+    }
+}
