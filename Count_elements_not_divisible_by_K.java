3514bc406dc60b61a11208db36ccf9734d83c749
KATHIPALLI MADHU KIRAN <119720465+madhukirankathipalli@users.noreply.github.com>
2023-06-15 10:45:25 +0530

    import java.util.*; public class count {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt(),c=0;         int k=sc.nextInt();         int a[]=new int[n];         for(int i=0;i<n;i++)         {             a[i]=sc.nextInt();             if(a[i]%k!=0)             {                 c++;             }         }         System.out.println(c);     } }

---
+++ Count_elements_not_divisible_by_K.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class count
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt(),c=0;
+        int k=sc.nextInt();
+        int a[]=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            a[i]=sc.nextInt();
+            if(a[i]%k!=0)
+            {
+                c++;
+            }
+        }
+        System.out.println(c);
+    }
+}
