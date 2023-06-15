cc98a47c7fcf02df77859400878e540c5aeb028e
KATHIPALLI MADHU KIRAN <119720465+madhukirankathipalli@users.noreply.github.com>
2023-06-15 11:18:16 +0530

    import java.util.*; public class sample {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         HashSet<Integer> a=new HashSet<Integer>();         for (int i=0;i<n;i++)             a.add(sc.nextInt());         System.out.print(n+(n-a.size()));     } }

---
+++ Count_of_sub_arrays_which_start_and_end_with_the_same_element..java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class sample
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        HashSet<Integer> a=new HashSet<Integer>();
+        for (int i=0;i<n;i++)
+            a.add(sc.nextInt());
+        System.out.print(n+(n-a.size()));
+    }
+}
