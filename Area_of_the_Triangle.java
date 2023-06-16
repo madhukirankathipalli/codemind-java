55d822f238381d1e07e911c7e5d59748a8cf0444
KATHIPALLI MADHU KIRAN <119720465+madhukirankathipalli@users.noreply.github.com>
2023-06-16 21:11:42 +0530

    import java.util.Scanner; public class main1 {     public static void main(String args[]){         Scanner sc=new Scanner(System.in);
            int a=sc.nextInt();
           int b=sc.nextInt();
           int c=sc.nextInt();
    
            double area =calculateTriangleArea(a,b,c);
        
            System.out.printf("%.2f",area);
        }
    
    private static double calculateTriangleArea(int a,int b, int c){
    double s=(a+b+c)/2.0;
    double area =Math.sqrt(s*(s-a)*(s-b)*(s-c));
    return area;
    }
    
    
    }

--- Area_of_the_Triangle.java
+++ Area_of_the_Triangle.java
@@ -1,23 +1,22 @@
 import java.util.Scanner;
+public class main1 {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        
+        int a=sc.nextInt();
+       int b=sc.nextInt();
+       int c=sc.nextInt();
 
-public class TriangleArea {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int a = scanner.nextInt(); // side a
-        int b = scanner.nextInt(); // side b
-        int c = scanner.nextInt(); // side c
-
-        double area = calculateTriangleArea(a, b, c);
-        System.out.printf("%.2f", area);
+        double area =calculateTriangleArea(a,b,c);
+    
+        System.out.printf("%.2f",area);
     }
 
-    private static double calculateTriangleArea(int a, int b, int c) {
-        // Calculate the semi-perimeter
-        double s = (a + b + c) / 2.0;
+private static double calculateTriangleArea(int a,int b, int c){
+double s=(a+b+c)/2.0;
+double area =Math.sqrt(s*(s-a)*(s-b)*(s-c));
+return area;
+}
 
-        // Calculate the area using Heron's formula
-        double area = Math.sqrt(s * (s - a) * (s - b) * (s - c));
 
-        return area;
-    }
 }
